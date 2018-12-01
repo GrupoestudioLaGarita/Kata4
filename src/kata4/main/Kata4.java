165409d94dbe0f09f107333e8ba642534f52630f
Carlos <Carlos@DESKTOP-TUCUS6J>
2018-12-01 15:22:53 +0000

    Kata4 v1

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -8,15 +8,13 @@ import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
 
 public class Kata4 {
-    
-    
+
     public static void main(String[] args) {
-    String fileName = "email.txt";
-    List<Mail> mailList = MailListReader.Read(fileName);
-    Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-    HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-    
-    histoDisplay.execute();
+        String fileName = "email.txt";
+        List<Mail> mailList = MailListReader.Read(fileName);
+        Histogram histogram = MailHistogramBuilder.build(mailList);
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
     }
-    
+
 }
